b58e7359e7bdbf362bfe8595b7e76b33bbf527b4
Michelle <angelamaincs@gmail.com>
2023-01-24 23:34:32 -0800

    flight finder 2

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from data_manager import DataManager
 from flight_search import FlightSearch
 from notification_manager import NotificationManager
 
-ORIGIN_CITY = "SF"
+ORIGIN_CITY_IATA = "LON"
 
 data_manager = DataManager()
 flight_search = FlightSearch()
@@ -13,28 +13,42 @@ sheet_data = data_manager.get_destination_data()
 
 if sheet_data[0]["iataCode"] == "":
     city_names = [row["city"] for row in sheet_data]
-    print(city_names)
-    codes = flight_search.get_destination_codes(city_names)
-    data_manager.update_destination_codes(codes)
+    data_manager.city_codes = flight_search.get_destination_codes(city_names)
+    data_manager.update_destination_codes()
     sheet_data = data_manager.get_destination_data()
 
-today = datetime.now() + timedelta(1)
-six_month_from_today = datetime.now() + timedelta(6*30)
+destinations = {
+    data["iataCode"]: {
+        "id": data["id"],
+        "city": data["city"],
+        "price": data["lowestPrice"]
+    } for data in sheet_data}
 
-for destination in sheet_data:
+tomorrow = datetime.now() + timedelta(days=1)
+six_month_from_today = datetime.now() + timedelta(days=6 * 30)
+
+for destination_code in destinations:
     flight = flight_search.check_flights(
-        ORIGIN_CITY,
-        destination["iataCode"],
-        from_time = today,
-        to_time = today,
-        to_time = six_month_from_today
+        ORIGIN_CITY_IATA,
+        destination_code,
+        from_time=tomorrow,
+        to_time=six_month_from_today
     )
+    print(flight.price)
+    if flight is None:
+        continue
 
-    if flight.price < destination["lowestPrice"]:
-        notification_manager.send_sms(
-            message = f"Low price alert! Only £{flight.price} to fly from {flight.origin_city}-{flight.origin_airport} "
-                      f"to {flight.destination_city}-{flight.destination_airport}, "
-                      f"from {flight.out_date} to {flight.return_date}."
-        )
+    if flight.price < destinations[destination_code]["price"]:
 
+        users = data_manager.get_customer_emails()
+        emails = [row["email"] for row in users]
+        names = [row["firstName"] for row in users]
 
+        message = f"Low price alert! Only £{flight.price} to fly from {flight.origin_city}-{flight.origin_airport} to {flight.destination_city}-{flight.destination_airport}, from {flight.out_date} to {flight.return_date}."
+
+        if flight.stop_overs > 0:
+            message += f"\nFlight has {flight.stop_overs} stop over, via {flight.via_city}."
+
+        link = f"https://www.google.co.uk/flights?hl=en#flt={flight.origin_airport}.{flight.destination_airport}.{flight.out_date}*{flight.destination_airport}.{flight.origin_airport}.{flight.return_date}"
+
+        notification_manager.send_emails(emails, message, link)
